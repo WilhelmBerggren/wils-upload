71150e7d120d5b898295da4ad6200ffdfeac7683
Wilhelm Berggren <wilhelm.berggren@hiberworld.com>
2023-06-03 15:19:28 +0200

    Allow more file types

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -8,7 +8,12 @@ const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({
+    image: { maxFileSize: "4MB" },
+    blob: { maxFileSize: "8MB" },
+    audio: { maxFileSize: "8MB" },
+    video: { maxFileSize: "8MB" },
+  })
     // Set permissions and file types for this FileRoute
     .middleware(async (req) => {
       // This code runs on your server before upload
